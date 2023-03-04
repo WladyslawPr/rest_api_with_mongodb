999caabb3a9d0581229471d737908e0ad44b218e
vlpr <user.waw3@gmail.com>
2023-03-04 15:11:22 +0100

    created frame

---
+++ src/main/java/com/example/rest_api_with_mongodb/RestApiWithMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.example.rest_api_with_mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiWithMongodbApplication {
+
+    public static void main (String[] args) {
+        SpringApplication.run(RestApiWithMongodbApplication.class, args);
+    }
+
+}
